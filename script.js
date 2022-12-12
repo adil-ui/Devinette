8b9a666244ffc3b3d8360fabd971b6b7ade85bdd
unknown <adilboussaleem@gmail.com>
2022-12-12 12:49:01 +0100

    add auth

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ let auth = document.querySelector(".auth");
 let game = document.querySelector(".game");
 let lose = document.querySelector(".lose");
 let success = document.querySelector(".success");
+let labelEasy = document.querySelector("#labelEasy");
+let labelMedium = document.querySelector("#labelMedium");
+let labelDifficult = document.querySelector("#labelDifficult");
+/*
+let easy = document.querySelector("#easy");
+let medium = document.querySelector("#medium");
+let difficult = document.querySelector("#difficult")
+*/
+let levelRadio = document.querySelectorAll('.levelRadio')
 let message = document.querySelector(".message");
 let btnValide = document.querySelector('#btn_valide')
 let btnValideNumber = document.querySelector('#btnValideNumber')
@@ -12,13 +21,47 @@ let ParaErreur  = document.querySelector('#ParaErreur')
 let pointsMessage = document.querySelector('#pointsMessage')
 let trialsMessage = document.querySelector('#trialsMessage')
 let score = document.querySelector('#score')
+let email = document.querySelector('#inputEmail')
+let password = document.querySelector('#inputPassword')
+let authError = document.querySelector('#authError')
 let rundomNumber
 let name;
 let minNumber
 let maxNumber
 let myPoint
 let myTrial
+authError.style.color ="red"
+score.innerText = 0
 
+btnValide.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (email.value && password.value) {
+        fetch('https://reqres.in/api/login',{
+            method: "POST",
+            headers :{ "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: email.value,
+                password : password.value
+            })
+        })
+        .then(response => response.json()).then(result => {
+            if(result.token){
+                localStorage.setItem("name", inputEmail.value);
+                auth.style.display = "none";
+                gamerName.innerText = localStorage.getItem("name");
+                game.style.display = "block";
+            }else{
+                authError.innerHTML = "User Not Found"
+            }
+        })
+        
+    }else {
+        authError.innerHTML = 'fill the empty fields'
+    }
+
+})
+
+/*
 btnValide.addEventListener('click', (event) => {
     event.preventDefault()
   if (inputName.value != "") {
@@ -29,6 +72,16 @@ btnValide.addEventListener('click', (event) => {
   }
 })
 
+if (localStorage.getItem("name")) {
+    auth.style.display = "none";
+    gamerName.innerText = localStorage.getItem("name");
+    game.style.display = "block";
+}
+*/
+
+if (localStorage.getItem("myScore")) {
+    score.innerText = localStorage.getItem("myScore");
+}
 
 
 function selectLevel(minNum, maxNum, points, trials) {
@@ -50,34 +103,55 @@ function selectLevel(minNum, maxNum, points, trials) {
 
 function easyLevel() {
     selectLevel(1, 10, 1, 3);
+    labelEasy.style.color ="#00ff00"
+    labelMedium.style.color = "#cbfd00"
+    labelDifficult.style.color = "#cbfd00"
+    labelEasy.style.fontWeight ="bold"
+    labelMedium.style.fontWeight = "normal"
+    labelDifficult.style.fontWeight = "normal"
     console.log(rundomNumber)
 
 }
 
 function mediumLevel() {
     selectLevel(10, 100, 3, 5);
+    labelMedium.style.color ="#00ff00"
+    labelEasy.style.color ="#cbfd00"
+    labelDifficult.style.color = "#cbfd00"
+    labelMedium.style.fontWeight ="bold"
+    labelEasy.style.fontWeight = "normal"
+    labelDifficult.style.fontWeight = "normal"
+
     console.log(rundomNumber)
 
 }
 
 function difficultLevel() {
     selectLevel(100, 1000, 5, 10);
+    labelDifficult.style.color ="#00ff00"
+    labelMedium.style.color ="#cbfd00"
+    labelEasy.style.color ="#cbfd00"
+    labelDifficult.style.fontWeight = "bold"
+    labelEasy.style.fontWeight = "normal"
+    labelMedium.style.fontWeight = "normal"
     console.log(rundomNumber)
 
 }
 
 btnValideNumber.addEventListener('click', (event) =>{
     event.preventDefault();
+    Array.from(levelRadio).forEach(elt => elt.disabled = true)
     if (numberEnter.value == rundomNumber) {
-       score.innerText = parseInt(score.innerText + 1) 
+        let scorePoint = parseInt(score.innerText) + myPoint
+       localStorage.setItem("myScore", scorePoint);
+       score.innerText = localStorage.getItem("myScore");
        game.style.display = 'none'
        success.style.display = 'block'
-    }else if(numberEnter.value > maxNumber){
+    }else if(numberEnter.value > maxNumber || numberEnter.value < minNumber){
         paraTrial.style.display = 'none'
         paraPoint.style.display = 'none'
         paraMain.style.color = "red";
-        paraMain.innerText = "Entrez un nombre entier entre 1 et 10";
-        myTrial = myTrial - 1;
+        paraMain.innerText = `Entrez un nombre entier entre ${minNumber} et  ${maxNumber}`;
         pointsMessage.innerText = "";
         trialsMessage.innerText = `Il vous reste ${myTrial} Essai`;
     }
@@ -103,10 +177,30 @@ btnYes.addEventListener('click', (event) =>{
     event.preventDefault()
     lose.style.display = 'none'
     game.style.display = 'block'
+    Array.from(levelRadio).forEach(elt => elt.disabled = false)
+    numberEnter.focus()
+window.location.reload()
+
 })
 
 btnNo.addEventListener('click', (event) =>{
     event.preventDefault()
     lose.style.display = 'none'
+    auth.style.display = 'block'
+})
+
+btnSuccessYes.addEventListener('click', (event) =>{
+    event.preventDefault()
+    success.style.display = 'none'
+    game.style.display = 'block'
+    Array.from(levelRadio).forEach(elt => elt.disabled = false)
+    numberEnter.focus()
+window.location.reload()
+
+})
+
+btnSuccessNo.addEventListener('click', (event) =>{
+    event.preventDefault()
+    success.style.display = 'none'
     auth.style.display = 'block'
 })
